db4cac1114249952b16b09fe012b0f937da17e2f
Данил Васильевых <vasilevykhd@icloud.com>
2025-02-11 11:51:28 +0400

    Переписал все функии из протокола

--- CardiographTest/Services/Controller/Controller.cs
+++ CardiographTest/Services/Controller/Controller.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO.Ports;
 using System.IO;
+using CardiographTest.Properties;
 
 namespace CardiographTest.Services.Controller
 {
@@ -35,7 +36,66 @@ namespace CardiographTest.Services.Controller
 
     #region Методы
 
-    public void 
+    /// <summary>
+    /// Отправка команды SerialPort.
+    /// </summary>
+    /// <param name="cmd_block_code">Код блока команд.</param>
+    /// <param name="cmd_code">Код команды.</param>
+    /// <param name="rw_flag">1.</param>
+    /// <param name="parameter_val">1.</param>
+    /// <param name="parametr_size">1.</param>
+    /// <exception cref="IOException">Плата не отвечает.</exception>
+    public void USB_TX_short_Msg(byte cmd_block_code, byte cmd_code, byte rw_flag, ref byte parameter_val, int parametr_size)
+    {
+      byte cnt;
+      short i_byte = 1;
+      byte CRC_val = 0;
+      byte byte_buf;
+      //string bytesWritten;
+      Parametr.usb_tx_cmd_packet[0] = Convert.ToByte(Parametr.USB_BYTE_START);
+      USB_Paket_Constructor(ref i_byte, ref CRC_val, (byte)cmd_block_code);
+      USB_Paket_Constructor(ref i_byte, ref CRC_val, (byte)cmd_code);
+      USB_Paket_Constructor(ref i_byte, ref CRC_val, (byte)rw_flag);
+
+      for (cnt = 0; cnt < parametr_size; cnt++)
+      {
+        byte_buf = (byte)(parameter_val + cnt);
+        USB_Paket_Constructor(ref i_byte, ref CRC_val, byte_buf);
+      }
+
+      USB_Paket_Constructor(ref i_byte, ref CRC_val, CRC_val); //CRC
+
+      Parametr.usb_tx_cmd_packet[i_byte++] = Parametr.USB_BYTE_STOP;
+
+      if (this.Port.IsOpen)
+      {
+        this.Port.Write(Parametr.usb_tx_cmd_packet, 0, i_byte);
+      }
+      else
+      {
+        this.Port.Close();
+        throw new IOException("Error writing to serial port\n");
+      }
+    }
+
+    /// <summary>
+    /// Формерования пакета данных для платы.
+    /// </summary>
+    /// <param name="i_byte">Какой байт по счетчику.</param>
+    /// <param name="CRC_val">CRC value.</param>
+    /// <param name="data_byte">Данные.</param>
+    public void USB_Paket_Constructor(ref short i_byte, ref byte CRC_val, byte data_byte)
+    {
+      CRC_val ^= data_byte;
+      if ((data_byte == Parametr.USB_BYTE_START) || (data_byte == Parametr.USB_BYTE_STOP) || (data_byte == Parametr.USB_BYTE_ESC) || (data_byte == Parametr.USB_BYTE_DUMMY0) || (data_byte == Parametr.USB_BYTE_DUMMY1))
+      {
+        Parametr.usb_tx_cmd_packet[i_byte] = Parametr.USB_BYTE_ESC;
+        data_byte = data_byte ^= 0x21;
+        i_byte++;
+      }
+      Parametr.usb_tx_cmd_packet[i_byte] = data_byte;
+      i_byte++;
+    }
 
     #region Методы подключения и отключения
 
