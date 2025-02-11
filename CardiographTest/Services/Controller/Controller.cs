60e2923f7b61c75d4951f948bc153e17c1ae8189
Данил Васильевых <vasilevykhd@icloud.com>
2025-02-11 09:50:32 +0400

    Начинаем с подключения и отключения

--- CardiographTest/Services/Controller/Controller.cs
+++ CardiographTest/Services/Controller/Controller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Ports;
 using System.IO;
 
 namespace CardiographTest.Services.Controller
@@ -14,21 +15,66 @@ namespace CardiographTest.Services.Controller
   {
     #region Поля и свойства 
 
-    private SerialPort Plata;
-    
+    private SerialPort port;
+
+    /// <summary>
+    /// COM порт.
+    /// </summary>
+    public SerialPort Port
+    {
+      get { return port; }
+      set
+      {
+        if(this.port == null)
+          throw new ArgumentNullException("port");
+        this.port = value;
+      }
+    }
+
     #endregion
 
-
-
     #region Методы
-    
-    
+
+    public void 
+
+    #region Методы подключения и отключения
+
+    /// <summary>
+    /// Подключение к Serial Port.
+    /// </summary>
+    public void Connected()
+    {
+      this.Port.Open();
+      if (!this.Port.IsOpen)
+        throw new IOException();
+    }
+
+    /// <summary>
+    /// Отключение к Serial Port.
+    /// </summary>
+    public void Disconnected()
+    {
+      if(this.Port.IsOpen)
+        this.Port.Close();
+      else
+        throw new IOException();
+    }
+
+    #endregion
 
     #endregion
 
     #region Конструкторы
 
-    public Controller() {}
+    public Controller(SerialPort port)
+    {
+      this.Port = port;
+    }
+
+    public Controller() 
+    {
+      this.Port = new SerialPort();
+    }
 
     #endregion
   }
